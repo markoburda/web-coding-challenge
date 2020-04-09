0af35bd6c91802af7b65284e090b19d7414abcf6
markoburda <burda@ucu.edu.ua>
2020-04-09 17:52:22 +0300

    Just made the code a tad bit cleaner

--- guitar_tuner.js
+++ guitar_tuner.js
@@ -1,10 +1,3 @@
-// 1 (E)	329.63 Hz	E4
-// 2 (B)	246.94 Hz	B3
-// 3 (G)	196.00 Hz	G3
-// 4 (D)	146.83 Hz	D3
-// 5 (A)	110.00 Hz	A2
-// 6 (E)	82.41 Hz	E2
-
 // - The guitar strings are played 1st to 6th, High E to Low E.
 // - If the guitar string matches, return `"OK"` for that guitar string.
 // - If it's too low, return `">•"` for 1 or 2 percent off (the arrow means, tune up).
@@ -23,17 +16,12 @@ function tune(arr) {
         else if(str === 0){return "-"}
         else{
             const diff = Math.round(100 * Math.abs(str - perfStr)/perfStr)/100;
-            if(str > perfStr){
-                return 0.03 > diff ? "•<" : "•<<";
-            }
-            else{
-                return 0.03 > diff ? ">•" : ">>•";
-            }
+            if(str > perfStr) return 0.03 > diff ? "•<" : "•<<";
+            else return 0.03 > diff ? ">•" : ">>•";
         }
     }
     const perfArr = [329.63, 246.94, 196.00, 146.83, 110.00, 82.41];
-    res = [];
-
+    let res = [];
     for(var i = 0; i < arr.length; i++){
         res[i] = cmp(arr[i], perfArr[i]);
     }
